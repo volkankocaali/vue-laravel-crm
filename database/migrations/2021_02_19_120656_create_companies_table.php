93f18ec2e40c91e2c36e6ef6b0e98befb0f7d2b7
Volkan Kocaali <volkan.kocaali@tardigrad.com.tr>
2021-02-21 22:39:53 +0300

    Sorumlu Ekip , müşteri tipi , status , boyut kodu tabloları #10

--- database/migrations/2021_02_19_120656_create_companies_table.php
+++ database/migrations/2021_02_19_120656_create_companies_table.php
@@ -16,10 +16,29 @@ class CreateCompaniesTable extends Migration
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
             $table->string('company_name');
-            $table->string('company_phone');
-            $table->string('company_author');
-            $table->string('company_web_site');
+            $table->string('company_address')->nullable();
+            $table->unsignedBigInteger('company_main_country')->nullable();
+            $table->unsignedBigInteger('company_country')->nullable();
+            $table->unsignedBigInteger('company_city')->nullable();
+            $table->string('company_phone')->nullable();
+            $table->string('company_author')->nullable();
+            $table->string('company_web_site')->nullable();
+            $table->string('tax_number')->nullable();
+            $table->string('sector')->nullable();
+            $table->string('field')->nullable();
+            $table->string('potential_fleet')->nullable();
             $table->string('is_active');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('status_id');
+            $table->unsignedBigInteger('customer_type_id');
+            $table->unsignedBigInteger('size_code_id');
+            $table->foreign('company_main_country')->references('id')->on('geozone_countries')->onDelete('cascade');
+            $table->foreign('company_country')->references('id')->on('geozone_countries')->onDelete('cascade');
+            $table->foreign('company_city')->references('id')->on('geozone_cities')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('status_id')->references('id')->on('statuses')->onDelete('cascade');
+            $table->foreign('customer_type_id')->references('id')->on('customer_types')->onDelete('cascade');
+            $table->foreign('size_code_id')->references('id')->on('size_codes')->onDelete('cascade');
             $table->timestamps();
         });
     }
